ef4880038b7dcd96dfb54e422ce53bee6e3a8990
Eric Tomb <eric.tomb@alaskaair.com>
2019-03-01 16:23:02 -0800

    add pynet week 1

---
+++ week1/exercise1.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+ip_addr1 = "192.168.1.1"
+ip_addr2 = "192.168.2.1"
+ip_addr3 = "192.168.3.1"
+
+print("My IP addresses are: " + "%s %s %s" % (ip_addr1, ip_addr2, ip_addr3))
